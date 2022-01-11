9a138b6a544d419c62c159e59879f5edefeb564a
LMR <3524613297@qq.com>
2022-01-11 23:43:54 +0800

    类

--- 2022-1-10/2022-1-10/class.cpp
+++ 2022-1-10/2022-1-10/class.cpp
@@ -3,25 +3,52 @@
 using namespace std;
 
 
+//class A
+//{
+//public:
+//	//类体：成员变量和成员函数组成
+//	void test()
+//	{
+//		cout << this << endl;
+//	}
+//	char c;
+//	int b;
+//};//注意，一定要带分号
+//class C
+//{
+//
+//};
 
-class A
+class Data
 {
-	//类体：成员变量和成员函数组成
-	void test();
-	char c;
-	int b;
-};//注意，一定要带分号
-class C
-{
+public:
+	//Data(int year, int month, int day)
+	//{
+	//	_year = year;
+	//	_month = month;
+	//	_day = day;
+	//}
+	//Data()
+	//{
 
+	//}
+	void Set(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
 };
-
-
 
 int main()
 {
-	cout << sizeof(A) << endl;
-	cout << sizeof(C) << endl;
+	Data d();
+	//d.Set(100, 12, 12);
 	system("pause");
 	return 0;
+	
 }
