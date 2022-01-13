1b8f2ce6154e9cb56b082199b9483ed4401a729f
LMR <3524613297@qq.com>
2022-01-13 23:03:11 +0800

    +拷贝构造函数

--- 2022-1-10/2022-1-10/class.cpp
+++ 2022-1-10/2022-1-10/class.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <Windows.h>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
-
+#define _CRT_SECURE_NO_WARNINGS
 
 //class A
 //{
@@ -18,7 +20,7 @@ using namespace std;
 //{
 //
 //};
-
+#if 0
 class Data
 {
 public:
@@ -55,4 +57,98 @@ int main()
 	system("pause");
 	return 0;
 	
+}
+#endif
+#if 0
+class Data
+{
+public:
+	Data(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+
+	Data(const Data d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+	void PrintfData()
+	{
+		cout << _year << " " << _month << " " << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+#endif
+#if 0
+class String
+{
+public:
+	String(const char* str = "jack")
+	{
+		_str = (char *)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+
+	~String()
+	{
+		free(_str);
+		_str = nullptr;
+	}
+private:
+	char* _str;
+};
+int main()
+{
+	//Data d1(2022, 1, 13);
+	//Data d2(d1);
+	//int a;
+	//int b = 10;
+	//int c = b;
+	//int d(c);
+	String s1("hello world");
+	String s2(s1);
+	system("pause");
+	return 0;
+}
+#endif
+class String
+{
+public:
+	//构造函数
+	String(const char* str = "jack")
+	{
+		_str = (char *)malloc(strlen(str) + 1);
+		strcpy(String::_str, str);
+	}
+	//拷贝构造函数
+	String(const string& s)
+	{
+		cout << "拷贝构造函数" << endl;
+	}
+
+	~String()
+	{
+		if (_str)
+		{
+			free(_str);
+			_str = nullptr;
+		}
+	}
+private:
+	char* _str;
+};
+int main()
+{
+	String s1("hello world");
+	String s2("Hello World");
+	s1 = s2;
+	system("pause");
+	return 0;
 }
