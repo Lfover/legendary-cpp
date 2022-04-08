27c6fd2677ad068328c6e7747e8e78b95a8171cd
LMR <3524613297@qq.com>
2022-04-08 21:18:54 +0800

    笔试调试的题

---
+++ 1111/1111/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+int main()
+{
+	unordered_map<string, bool>;
+	string s;
+	while (cin >> s)
+	{
+		string s1 = strstr(s, )
+	}
+	return 0;
+}
