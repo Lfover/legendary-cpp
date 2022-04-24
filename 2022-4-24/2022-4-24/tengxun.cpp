354dcf89bef23fa22aff3f447cd931fca3639b9f
LMR <3524613297@qq.com>
2022-04-24 23:02:59 +0800

    腾讯

---
+++ 2022-4-24/2022-4-24/tengxun.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//int n;
+//vector<string> s;
+//int main()
+//{
+//	cin >> n;
+//	string s2;
+//	string s3;
+//	vector<string> s1;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> s3;
+//		s.push_back(s3);
+//	}
+//	int n2 = s[0].size();
+//	for (int i = 0; i < n2; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//		{
+//			s2 += s[j][i];
+//		}
+//		int n1 = s2.find_first_not_of('0');
+//		s1.push_back(s2.substr(n1, s2.size()));
+//		string().swap(s2);
+//	}
+//		sort(s1.begin(), s1.end());
+//	int size = s1.size();
+//	for (int i = 0; i < size; i++)
+//	{
+//		cout << s1[i] << ' ';
+//	}
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//}
+ListNode * head = new (-1)
